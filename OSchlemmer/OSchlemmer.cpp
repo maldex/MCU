63ba48020d94f907cc9ebc24c2e4e62fd0037793
Arudino_win_xp <arduino@WinXP>
2016-09-29 00:59:54 +0200

    total refactor: using struct for scene data

--- OSchlemmer/OSchlemmer.cpp
+++ OSchlemmer/OSchlemmer.cpp
@@ -1,168 +1,166 @@
 #include "Arduino.h"
 #include "Servo.h"
-#include "EEPROM.h"
-/* https://github.com/maldex/MCU/tree/master/OSchlemmer
+
+/*
+ * Object, Pin and value definitions
  */
 
-
-class CustomServo: public Servo {
-private:
-	uint8_t low;
-	uint8_t high;
-	uint8_t speed;
-public:
-	int pin;
-	void getstarted();
-	void maintain();
-	void calibrate(uint8_t analog_pin);
-	void enable(uint8_t v);
-	void setState();
-	void setState(bool state);
-};
-
-void CustomServo::getstarted() {
-	low = EEPROM.read(pin * 10);
-	high = EEPROM.read(pin * 10 + 1);
-	speed = EEPROM.read(pin * 10 + 2);
-}
-
-void CustomServo::maintain() {
-//
-}
-
-void CustomServo::enable(uint8_t v) {
-	this->attach(pin);
-	this->write(v);
-//	lastmillis = millis();
-}
-
-void CustomServo::setState(bool state) {
-	if (state) {
-		this->enable(high);
-	}else{
-		this->enable(low);
-	}
-}
-
-void CustomServo::calibrate(uint8_t analog_pin){
-	Serial.print("'l'ow or 'h'igh value, adjust via poti on pin "); Serial.println(analog_pin);
-	this->attach(pin);
-	int lvalue;
-	while (true){
-		delay(10);
-		int nvalue = analogRead(analog_pin) * 180.0 / 1023.0;;
-		uint8_t value = nvalue; //
-		if (lvalue != nvalue ){
-			this->write(value);
-			Serial.print("Servo on PIN "); Serial.print(pin); Serial.print(" - "); Serial.println(value);
-			lvalue = nvalue;
-		}
-
-		if (Serial.available()) {
-			char c = Serial.read();
-			if (c == 'l'){
-				EEPROM.write(pin * 10, value);
-				Serial.print("l written to ");Serial.println(pin * 10);
-			}
-			else if (c == 'h'){
-				EEPROM.write(pin * 10 + 1, value);
-				Serial.print("h written to ");Serial.println(pin * 10 + 1);
-			}
-			else if ( c == 's'){
-				Serial.println("enter speed in 0-255: ");
-				uint8_t speed = Serial.parseInt();
-				EEPROM.write(pin * 10 + 2, speed);
-				Serial.print(speed);Serial.print("s written to ");Serial.println(pin * 10 + 2);
-			}
-		}
-	}
-}
-
-
-/* ------------------------------------------------------------ here we start */
-
-
-
-
-/* pins used in this program */
+// input pins
 const int button1 =	A5;
 const int poti1 =	A4;
 const int poti2 =	A3;
-const int led1 =	9;
-const int led2 =	10;
-const int led3 = 	11;
 
+// leds
+const uint8_t fakelPin 	= 11;
+const uint8_t ledPin[5] = { 2, 3,   8, 9, 10 };  // digital out Pins on Arduino
 
-unsigned long lmillis;
+// servos
+const uint8_t servoPin[4] =      {   4,   5,   6,   7 };  // digital out Pins on Arduino
+// calibration data
+const uint8_t servoCalibMin[4] = {   0,  20,  40,  60 };  // one side
+const uint8_t servoCalibMax[4] = { 180, 160, 140, 120 };  // the other side
 
-void myirq(){
-	if ( millis() - lmillis > 100 ){
-		lmillis = millis();
-		digitalWrite(led1,!digitalRead(led1));
+// arduino servo objects
+Servo ServoObj[4];
+
+/*
+ * Scene related definitions and data
+ */
+
+/* scene related definitions */
+struct Scene {   /* definition of scene-data structure */
+	long scene_duration;  //in milliseconds
+	bool fakel;
+	uint8_t servo0;
+	uint8_t servo1;
+	uint8_t servo2;
+	uint8_t servo3;
+	uint8_t led0;
+	uint8_t led1;
+};
+
+Scene scene0 = { /* default power-on scene */
+		-1,					// scene_duration
+		false,				// fakel
+		servoCalibMin[0],	// servo0
+		servoCalibMin[1],	// servo1
+		servoCalibMin[2],	// servo2
+		servoCalibMin[3],	// servo3
+		0,					// led0
+		0,					// led1
+};
+Scene scene1 = {
+		1000,				// scene_duration
+		true,				// fakel
+		servoCalibMin[0],	// servo0
+		servoCalibMax[1],	// servo1
+		servoCalibMax[2],	// servo2
+		servoCalibMin[3],	// servo3
+		255,				// led0
+		255,				// led1
+};
+Scene scene2 = {
+		2500,				// scene_duration
+		true,				// fakel
+		servoCalibMax[0],	// servo0
+		servoCalibMax[1],	// servo1
+		servoCalibMin[2],	// servo2
+		servoCalibMax[3],	// servo3
+		0,					// led0
+		128,				// led1
+};
+
+unsigned long lastSceneTransition;
+volatile uint8_t currentSceneCounter = -1;
+Scene currentScene = scene0;
+
+/*
+ * Functions
+ */
+void scene_transition(){
+	currentSceneCounter += 1;
+	if (currentSceneCounter > 2 )
+		currentSceneCounter=0;
+	switch(currentSceneCounter) {
+	case 0:
+		currentScene = scene0;
+		break;
+	case 1:
+		currentScene = scene1;
+		break;
+	case 2:
+		currentScene = scene2;
+		break;
+	}
+
+	Serial.print("Transitioning into scene ");	Serial.print(currentSceneCounter);
+	Serial.print(" for ");	Serial.println(currentScene.scene_duration);
+	lastSceneTransition = millis(); // this will cause stall/rounding issues after few up-days
+
+	ServoObj[0].write(currentScene.servo0);
+	ServoObj[1].write(currentScene.servo1);
+	ServoObj[2].write(currentScene.servo2);
+	ServoObj[3].write(currentScene.servo3);
+
+	analogWrite(ledPin[0], currentScene.led0);
+	analogWrite(ledPin[1], currentScene.led1);
+}
+
+void calibrate() {
+	Serial.println("Calibration: use poti1 for ALL servos!");
+	while (true) {
+		int raw = analogRead(poti1);
+		int val = raw * 180.0 / 1023.0 ;
+		for (int i=0; i < sizeof(servoPin); i++){
+			ServoObj[i].write(val);
+		}
+		Serial.print(raw); Serial.print(" -> "); Serial.print(val);Serial.println('°');
+		delay(25);
 	}
 }
 
-uint8_t ServoPins[4] = { 5,7,6,4 };
-CustomServo ServoObj[4];
-/* finally: the arduino style main functions */
+/*
+ * finally here we go: the arduino standart functions
+ */
 void setup() {
+	Serial.begin(115200); Serial.println("hi");
+
 	// setup pinmodes
 	pinMode(button1, INPUT);
 	pinMode(poti1, INPUT);
 	pinMode(poti2, INPUT);
-	pinMode(led3, OUTPUT);
-	pinMode(led2, OUTPUT);
-	pinMode(led1, OUTPUT);
-
-	Serial.begin(115200);
-	Serial.println("hi");
-	for (int i=0; i < sizeof(ServoPins); i++){
-		ServoObj[i].pin = ServoPins[i];
-		ServoObj[i].getstarted();
+	pinMode(fakelPin, OUTPUT);
+	for (int i=0; i < sizeof(ledPin); i++){
+		pinMode(ledPin[i], OUTPUT);
 	}
 
+	// attaching Servos
+	for (int i=0; i < sizeof(servoPin); i++){
+		ServoObj[i].attach(servoPin[i]);
+	}
+
+	// enter calibration mode: hold button1 while pressing reset
 	if (digitalRead(button1)){
-			Serial.println("Calibration: enter number of Servo: ");
-			while (! Serial.available()) delay(10);
-			uint8_t n = Serial.read();
-			Serial.flush(); // empty remaining buffer
-			n = n - 48;
-			if (n > sizeof(ServoPins)) {
-				Serial.print("unknown servo "); Serial.println(n);
-			} else {
-				ServoObj[n].calibrate(poti1);
-			}
+		calibrate();
 	}
-	Serial.println("starting");
 
+	// transitioning into scene 0 (from -1)
+	scene_transition();
 }
 
-bool state = false;
 void loop() {
-	if (state) {
-		analogWrite(led3, random(0, analogRead(poti1) ));
+	if (currentScene.fakel) {
+		// flicker fackel with intensity of poti1
+		analogWrite(fakelPin, random(0, analogRead(poti1) ));
 		delay(random(0,10));
 	}
 
-	if (digitalRead(button1)){
-		state = (state == false);
-		analogWrite(led3,0);
-		while(digitalRead(button1)) delay(10); // cheap debounce
-		Serial.print("button pressed ");Serial.println(state);
-		ServoObj[0].setState(state);
-		digitalWrite(led2, state);
-//		for (int i=0; i < sizeof(ServoPins); i++){
-//			ServoObj[i].setState(state);
-//		}
+	// scene transition: if button was pressed or scene-time runs out or ???
+	if (digitalRead(button1) or
+			( (millis() - lastSceneTransition) > currentScene.scene_duration) ){
+
+		while(digitalRead(button1)) delay(10); // cheap debounce - make sure button1 is NOT pressed
+
+		scene_transition();
 	}
-	myirq();
-
-	uint8_t sv = analogRead(poti2) *  180.0 / 1023.0 ;
-
-	ServoObj[1].enable( sv );
-
-//	for (int i=0; i++; i < sizeof(ServoPins)){
-//		ServoObj[i].turnOn(analogRead(poti1)* 180.0 / 1024.0 );
-//		ServoObj[i].maintain();
-//	}
 }
