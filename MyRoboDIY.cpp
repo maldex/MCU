677ce387e24aab87b848912031a8ca58eb174d10
Arudino_win_xp <arduino@WinXP>
2018-03-25 00:52:25 +0100

    added LCD

--- MyRoboDIY.cpp
+++ MyRoboDIY.cpp
@@ -48,31 +48,49 @@ void EEPROMWritelong(int address, long value){
 	EEPROM.write(address + 3, one);
 }
 
+
+
+///////////////////////////////////////////////
+// state machinery
+///////////////////////////////////////////////
+#define STATE_DAZZLED	0
+#define STATE_EXPLORING	1
+#define STATE_ON_LINE	2
+#define STATE_OFF_LINE	3
+#define STATE_REVERSING	4
+
+uint16_t gc = 0;  // global counter
+int8_t state = STATE_EXPLORING;
+long last_state_change = 0, now = 0, state_age = 0, delta = 0;
+float steer = 0;
+
+
 ///////////////////////////////////////////////
 // Class for Servo with calibration in eeprom
 ///////////////////////////////////////////////
-Servo		motor_left, motor_right;
 class Drive2Wd
 {
 public:
+	Servo		motor_left, motor_right;
+
 	uint16_t	null_left, null_right;
-	int8_t		should_left, should_right;
+	uint16_t	should_left, should_right;
 
 	Drive2Wd(){ /* empty constructor, we'll start with begin(), stack/heap/instance thing on MCU */};
 	void begin(uint8_t pin_left, uint8_t pin_right){
 		get_calibration();
-		motor_left.attach(pin_left);				motor_right.attach(pin_right);
-		motor_left.writeMicroseconds(null_left);	motor_right.writeMicroseconds(null_right);
+		motor_left.attach(pin_left);					motor_right.attach(pin_right);
+		motor_left.writeMicroseconds(null_left);		motor_right.writeMicroseconds(null_right);
 	};
 
 	/* exact 0-value of servo is stored in eeprom */
 	void get_calibration(){
-		null_left = EEPROMReadlong(986);			null_right = EEPROMReadlong(996);
+		null_left = EEPROMReadlong(986);				null_right = EEPROMReadlong(996);
 	};
 
 	void set_calibration(){
-		null_left = motor_left.readMicroseconds(); 	null_right=motor_right.readMicroseconds();
-		EEPROMWritelong(986, null_left);			EEPROMWritelong(996, null_right);
+		null_left = motor_left.readMicroseconds(); 		null_right=motor_right.readMicroseconds();
+		EEPROMWritelong(986, null_left);				EEPROMWritelong(996, null_right);
 	};
 
 	int calc_servo_timing(int value, int calib) {
@@ -81,11 +99,35 @@ public:
 		if (value < 0) return map(value, -512, 0, MIN_PULSE_WIDTH, calib);
 	};
 
-	void write(int left, int right){
-		left = calc_servo_timing(-left, null_left);	right = calc_servo_timing(right, null_right);
-		motor_left.writeMicroseconds(left);			motor_right.writeMicroseconds(right);
+	void drive(int velo, int steering, int max_velo){
+		int left = float(map(steering, -512, 512, -max_velo, max_velo));
 	};
 
+
+	void write(int left, int right){
+		should_left = calc_servo_timing(-left, null_left);		should_right = calc_servo_timing(right, null_right);
+		if (left == 0) {
+			motor_left.writeMicroseconds(null_left);
+			should_left = null_left;
+		}
+		if (right == 0) {
+			motor_right.writeMicroseconds(null_right);
+			should_right = null_right;
+		}
+		maintain();
+	};
+
+	void maintain(){
+		int accel = delta;
+		int left = motor_left.readMicroseconds();
+		int right = motor_right.readMicroseconds();
+		if ( left < should_left) { left+=accel; }
+		if ( left > should_left) { left-=accel; }
+		if ( right < should_right) { right+=accel; }
+		if ( right > should_right) { right-=accel; }
+		motor_left.writeMicroseconds(left);
+		motor_right.writeMicroseconds(right);
+	}
 };
 // instanciate my class - once
 Drive2Wd My2Wd;
@@ -117,33 +159,38 @@ bool col_left = false, col_right = false;
 void interrupt_left(){ col_left = true; }
 void interrupt_right(){ col_right = true; }
 
-///////////////////////////////////////////////
-// state machinery
-///////////////////////////////////////////////
-#define STATE_DAZZLED	0
-#define STATE_EXPLORING	1
-#define STATE_ON_LINE	2
-#define STATE_OFF_LINE	3
-#define STATE_REVERSING	4
-
-#define threshold_trig  4
-
-uint16_t gc = 0;  // global counter
-int8_t state = STATE_EXPLORING, threshold = 0;
-long last_state_change = 0, now = 0, state_age = 0, delta = 0;
 
 void reportStatus(){
 	Serial.flush();
-	Serial << F("Status/age/FreeMem/delta | Servo left/right | Sensor s/t/v/thr: ") << state << '/' << state_age << '/' << freeMemory() << '/' << delta << F(" | ") << motor_left.readMicroseconds() << '/' << motor_right.readMicroseconds() << F(" | ") << s_value << '/' << t_value << '/' << v_value << '/' << threshold << '\n';
+//	Serial.write(0xFE); Serial.write(0x51); //LCD: clear screen
+	Serial.write(0xFE); Serial.write(0x45); Serial.write(0x00); //LCD: 1st line
+	Serial << F("state/a/d: ") << state << '/';
+	if (state_age < 1000) Serial.print('0');
+	if (state_age < 100) Serial.print('0');
+	if (state_age < 10) Serial.print('0');
+	Serial << state_age << '/' <<  delta;
+	Serial.write(0xFE); Serial.write(0x45); Serial.write(0x40); //LCD: 2nd line
+	Serial << delta << F("mot: ") << My2Wd.motor_left.readMicroseconds() << '/' << My2Wd.motor_right.readMicroseconds() << '/'<< steer;
+	Serial.write(0xFE); Serial.write(0x45); Serial.write(0x14); //LCD: 3rd line
+	Serial << F("s/t/v: ") << s_value << '/' << t_value << '/' << v_value;
+	Serial.write(0xFE); Serial.write(0x45); Serial.write(0x54); //LCD: 4th line
+
+//	Serial << F("Status/age/FreeMem/delta | Servo left/right/steer | Sensor s/t/v: ") << state << '/' << state_age << '/' << freeMemory() << '/' << delta << F(" | ") << My2Wd.motor_left.readMicroseconds() << '/' << My2Wd.motor_right.readMicroseconds() << '/'<< steer << F(" | ") << s_value << '/' << t_value << '/' << v_value << '\n';
+
+
+
 }
 
 void set_new_state(uint8_t new_state) {
 	reportStatus();
-	Serial << F("State change ") << state << F(" -> ") << new_state << F(" after ") << millis() - last_state_change << '\n';
+//	Serial << F("State change ") << state << F(" -> ") << new_state << F(" after ") << millis() - last_state_change << '\n';
+	Serial.write(0xFE); Serial.write(0x51); //LCD: clear screen
+
+	Serial.write(0xFE); Serial.write(0x45); Serial.write(0x54); //LCD: 4th line
+	Serial << F("CHANGE ") << state << F(" -> ") << new_state << F(" ") << millis() - last_state_change;
 	state=new_state, last_state_change=millis();
 	digitalWrite(pin_led_left,	LOW);
 	digitalWrite(pin_led_right, LOW);
-	My2Wd.write(0, 0);
 }
 
 
@@ -151,8 +198,13 @@ void set_new_state(uint8_t new_state) {
 // Arduino: setup
 ///////////////////////////////////////////////
 void setup(){
-	Serial.begin(115200); Serial << F("on poweron: press left to calibrate servos, press right to debug sensors") << '\n';
+	Serial.begin(9600);  // https://www.newhavendisplay.com/specs/NHD-0420D3Z-FL-GBW.pdf
+//	Serial.write(0xFE); Serial.write(0x61); Serial.write(0x08) ;	//LCD: set 115200 baud
+//	Serial.begin(115200);
+//		Serial.write(0xFE); Serial.write(0x61); Serial.write(0x04) ;	//LCD: set 9600 baud
 
+	Serial.write(0xFE); Serial.write(0x51); Serial.write(0x13);//LCD: clear screen
+	Serial.print(F("left motors, right sensors"));
 	// setup arduino pins
 	pinMode(pin_col_left, INPUT); pinMode(pin_col_left, INPUT);
 	pinMode(pin_led_left, OUTPUT); pinMode(pin_led_right, OUTPUT); pinMode(pin_led_3rd, OUTPUT);
@@ -202,36 +254,43 @@ void setup(){
 ///////////////////////////////////////////////
 // Arduino: loop
 ///////////////////////////////////////////////
+bool above_threshold; uint16_t readings;
 void loop(){
 	// do some timing
 	gc++;
-	delta = 10 -(millis() - now);   // sync to 10millis
-	if (delta > 0) delay( delta );
+	delta = 5 -(millis() - now);   // sync to 10millis
+	if (delta > 0) delay( delta ); else delta = 0;
 	now = millis();
 
 	// was there an collision?
 	if (col_left or col_right) {
+		My2Wd.write(0, 0);
 		set_new_state(STATE_REVERSING);
 		col_left = false; col_right = false;
 	}
 
 	// go on with my daily business
 	readSensors();
-	v_value /= 2;   // only use positive 0-512
-	float steer = 0;   // %tage of left/right steering when online
-	if (gc%100==0){
-		reportStatus();
+	v_value /= 8;   // only use positive 0-127
+
+	readings += s_value;
+	if (gc%4 == 0) {
+		above_threshold = readings >= t_value * 4;
+		readings = 0;
 	}
 
-	if ((s_value >= t_value) and threshold < threshold_trig) {threshold++; }
-	else if (threshold > -threshold_trig ){ threshold--; }
-	bool above_threshold = (threshold >= threshold_trig);
+	digitalWrite(pin_led_3rd, (above_threshold));
 
 
-	digitalWrite(pin_led_3rd, (threshold > 0));
 
 	// execute state-machine
 	state_age = now - last_state_change;
+	steer = (state_age / 333.0);
+	if (steer > 1.0) steer = 1.0;
+
+	if (gc%25==0){
+		reportStatus();
+	}
 	switch (state) {
 	case STATE_DAZZLED:
 		My2Wd.write(0,0);
@@ -245,40 +304,43 @@ void loop(){
 	case STATE_EXPLORING:
 		digitalWrite(pin_led_left,!(gc%4));
 		if (above_threshold) {
+			My2Wd.write(0,0);
 			set_new_state(STATE_ON_LINE);
 		}
 		if (state_age > 1000){ // reset to my own state
 			set_new_state(STATE_EXPLORING);
-			My2Wd.write(random(0,v_value), random(0,v_value));
+			My2Wd.write(random(-v_value,v_value*2), random(-v_value,v_value*2));
 		}
 		break;
 	case STATE_ON_LINE:
 		digitalWrite(pin_led_right,!(gc%4));
-		My2Wd.write(v_value , float(v_value*steer));
-		if ( ! above_threshold and state_age > 100) { // line lost 10ms ago
+		My2Wd.write(v_value , float(v_value*(1-steer)));
+		if ( ! above_threshold and state_age > 10) { // line lost 10ms ago
 			set_new_state(STATE_OFF_LINE);
 		}
 		break;
 	case STATE_OFF_LINE:
 		digitalWrite(pin_led_right,!(gc%4));
-		My2Wd.write(float(v_value * steer), v_value);
-		if ( above_threshold and state_age > 100) { // line lost 10ms ago
+		My2Wd.write(float(v_value * (1-steer)), v_value);
+		if ( above_threshold and state_age > 100) { // line found 10ms ago
 			set_new_state(STATE_ON_LINE);
 		}
-		if ( state_age > 2000 ){
+		if ( state_age > 3500 ){
+			My2Wd.write(0, 0);
 			set_new_state(STATE_REVERSING);
 		}
 		break;
 	case STATE_REVERSING:
 		digitalWrite(pin_led_left, (now%200 >= 100));
 		digitalWrite(pin_led_right, (now%200 <= 100));
-		if (state_age > 300 ) {
-			My2Wd.write(-v_value,-v_value);
-			if (above_threshold or state_age > 2000) { set_new_state(STATE_DAZZLED);}
+		if (state_age > 250 ) {
+			My2Wd.write(-v_value*2,-v_value*2);
+			if (above_threshold or state_age > 1500) { set_new_state(STATE_DAZZLED);}
 		}
 		break;
 	default:
 		Serial.println("unknown state");
 	}
+	My2Wd.maintain();
 	/* That's all Folks */
 }
