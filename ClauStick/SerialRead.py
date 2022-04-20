6c5f3b92d537de2b54274cfbba7cf846f1653529
Josh Geisser <josh geisser@>
2022-04-20 15:12:55 +0200

    incl. pycode

--- ClauStick/SerialRead.py
+++ ClauStick/SerialRead.py
@@ -1,54 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 
 import serial   # pip install pyserial
-import threading
-import time
-
-class ClauStick(threading.Thread):
-    def __init__(self, serial_port='//./COM5', baud_rate=115200):
-        super().__init__()  # construct thread
-        self._running = False
-        self.serial_port, self.baud_rate = serial_port, baud_rate
-
-
-    def run(self):
-        self._running = True
-        try:
-            self.comport = serial.Serial(port=self.serial_port, baudrate=self.baud_rate)
-        except Exception as e:
-            raise(e)
-            self.stop()
-
-        while self._running:
-            self.read_from_serial()
-
-    def stop(self):
-        self._running = False
-
-    def read_from_serial(self):
-        try:
-            line = self.comport.readline().decode().strip()
-            self.values = {}
-            for word in line.split(' '):
-                print(word.strip().split(':'))
-                # self.values[key] = value
-        except Exception as e:
-            print("ERROR READING COMPORT " + str(e))
+from pprint import pprint
 
 if __name__ == "__main__":
-    try:
-        print("hi there")
+    comport = serial.Serial(port='//./COM5', baudrate=115200)
 
-        MyStick = ClauStick()
-        MyStick.start()
+    while True:
+        line = comport.readline().decode().strip()
+        data = {}
+        try:
+            for word in line.split(' '):      # split line by blanc
+                key, value = word.split(':')  # split word by ':'
+                data[key] = int(value)
 
-        while True:
-            print("main thread")
-            time.sleep(4)
+            pprint(data)
 
-
-    except KeyboardInterrupt as e:
-        MyStick.stop()
-        raise(e)
+        except Exception as e:
+            print(F"WARN: could not parse line '{line}'")
