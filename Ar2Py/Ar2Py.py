816da1daf802772f29864ed7cd9fbc4fba964585
tschoschy <tschoschy@gebaschtel.bnet>
2022-07-12 22:52:18 +0200

    simplified

--- Ar2Py/Ar2Py.py
+++ Ar2Py/Ar2Py.py
@@ -1,72 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Sinmple example how to communicate between arduino and python
-#
-# requires pyserial  "pip install pyserial"
-
-import os, serial, logging, threading, subprocess
-from time import sleep
-from pprint import pprint
-
-
-class SerialReader(threading.Thread):
-    def __init__(self, port="'//./COM3", baudrate=115200):
-        super().__init__()
-        logging.info(F"opening '{port}' at '{baudrate}")
-        self.ser = serial.Serial(port='//./COM3', baudrate=115200)
-        self.buffer = []
-
-    def run(self):  # thread
-        self.running = True
-        logging.info(F"Starting reader thread")
-        while self.running:
-            line = self.ser.readline().decode('ascii').strip()
-            logging.debug(F"read line '{line}'")
-            self.buffer.append(line)
-        logging.info(F"Stopping reader thread")
-
-    def stop(self):
-        self.running = False
-        logging.info(F"closing serial port")
-        self.ser.close()
-
-    def available(self):
-        return len(self.buffer) != 0
-
-    def read(self):
-        return self.buffer.pop()
-
-    def write(self, line):
-        logging.debug(F"write line '{line}'")
-        line += '\n'
-        self.ser.write(line.encode('ascii'))
-
+import subprocess, time, serial    # pip install pyserial
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    comport = serial.Serial(port='//./COM4', baudrate=115200)
 
-    Arduino = SerialReader()
-    Arduino.start()
+    while True:
+        try:
+            line = comport.readline().strip().decode()
+            print(line)
+            if line == "idle":              # skip idle messages
+                continue
 
-    ledstate = False
+            pin, value = line.split(':')    # expect line to be like   2:0  or 2:1
 
-    try:
-        while True:
-            while Arduino.available():
-                if Arduino.read() == "2:1":
-                    while Arduino.available(): Arduino.read()  # drain queue = simple debouncing
-                    print("------ TRIGGER ---------")
+            if value == '1':
+                subprocess.Popen("notepad.exe", close_fds=True)
+            if value == '0':
+                subprocess.Popen("taskkill /im notepad.exe")
 
-                    subprocess.Popen("notepad.exe", close_fds=True)
-
-
-
-            # blink LED
-            Arduino.write(F"LED {ledstate}")
-            ledstate = not ledstate
-
-            sleep(1)
-
-    except KeyboardInterrupt:
-        Arduino.stop()
+            time.sleep(1)
+            flush = comport.read_all()  # flush any buffer
+        except KeyboardInterrupt:
+            break
