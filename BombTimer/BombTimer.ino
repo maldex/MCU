2cc13c8f33f949ff86bf9fb2e90c6c9b42131205
tschoschy <tschoschy@gebaschtel.bnet>
2022-08-22 02:46:59 +0200

    BombTimer

---
+++ BombTimer/BombTimer.ino
@@ -0,0 +1,135 @@
+#include "Arduino.h"
+#include "EEPROM.h"
+#include "Adafruit_LEDBackpack.h"
+
+
+
+
+#define btn_tl A1  // button top left (1)
+#define btn_tr A0  // button top right (3)
+#define btn_bl A3  // button bottom left (*)
+#define btn_br A2  // button bottom right (#)
+#define led 13     // onboard led at pin 13
+
+#define value_addr 0xFA  // EEPROM address of value
+#define brigt_addr 0xFB  // EEPROM address of value
+
+enum status {
+	idle,
+	starting,
+	counting,
+	finished
+};
+
+Adafruit_7segment Display = Adafruit_7segment();
+status state = idle;
+
+void setTimerValue(){
+	Serial.println("entering set timer");
+	uint8_t value = EEPROM.read(value_addr);
+	Display.blinkRate(1);
+	while (true) {  // loop forever
+		if (digitalRead(btn_tr)) value++;
+		if (digitalRead(btn_tl)) value--;
+		if (value > 100) value = 100;
+		Display.print((int)value); Display.writeDisplay();
+		delay(100);
+		EEPROM.write(value_addr, value);
+	}
+}
+
+void setBrightness(){
+	Serial.println("entering set timer");
+	uint8_t value = EEPROM.read(brigt_addr);
+	Display.blinkRate(1);
+	while (true) {  // loop forever
+		if (digitalRead(btn_tr)) value++;
+		if (digitalRead(btn_tl)) value--;
+		if (value > 16) value = 16;
+		Display.setBrightness(value);
+		Display.print((int)value); Display.writeDisplay();
+		delay(100);
+		EEPROM.write(brigt_addr, value);
+	}
+}
+
+void setup()
+{
+	Serial.begin(9600); Serial.println("7 Segment BombTimer");
+
+	pinMode(btn_tl, INPUT);
+	pinMode(btn_tr, INPUT);
+	pinMode(btn_bl, INPUT);
+	pinMode(btn_br, INPUT);
+	pinMode(led, OUTPUT);
+
+	Display.begin(0x73);
+	Display.setBrightness(EEPROM.read(brigt_addr));
+
+	if (digitalRead(btn_br)) { setTimerValue();	}
+	if (digitalRead(btn_bl)) { setBrightness();	}
+
+
+}
+
+// The loop function is called in an endless loop
+int counter = 0;
+
+
+void displayPrint(int value, bool colon = true){
+
+	switch (value) {
+	case INT16_MAX:
+		// just print dashes
+		//		Display.print(-9999);
+		for (int digit = 0; digit < 5; digit++) { Display.writeDigitRaw(digit, 0x40); }
+		break;
+	case INT16_MIN:
+		// clear the display
+		for (int digit = 0; digit < 5; digit++) { Display.writeDigitRaw(digit, 0x00); }
+		break;
+	default:
+		// print number leading zero
+		Display.print(value);
+		if (value<10000) Display.writeDigitNum(0, 0);
+		if (value<1000) Display.writeDigitNum(1, 0);
+		if (value<100) Display.writeDigitNum(2, 0);
+		if (value<10) Display.writeDigitNum(3, 0);
+		break;
+	}
+	Display.drawColon(colon);
+	Display.writeDisplay();
+}
+
+void loop()
+{
+	//	if (digitalRead(btn_tl)){Display.print(1111); Display.writeDisplay(); return; }
+	//	if (digitalRead(btn_tr)){Display.print(3333); Display.writeDisplay(); return; }
+	//	if (digitalRead(btn_bl)){Display.print(7777); Display.writeDisplay(); return; }
+	//	if (digitalRead(btn_br)){Display.print(9999); Display.writeDisplay(); return; }
+
+
+	unsigned long millisNow = millis();
+	counter++;
+
+	switch (state){
+	case idle:
+		digitalWrite(led, false);
+		displayPrint(INT16_MAX); // just dashes
+		if (digitalRead(btn_tl)) {state = finished;}
+		break;
+
+	case finished:
+		digitalWrite(led, true);
+		if (counter%2){ displayPrint(0); }
+		else { displayPrint(INT16_MIN, false); }
+
+		delay(100);
+		if (digitalRead(btn_bl)) {state = idle;}
+
+		break;
+
+	default: break;  // should never happen
+	}
+
+}
